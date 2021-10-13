1134476274d858dfcfd76b5522fc3cd88313c376
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-13 11:09:42 +0100

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import './style.css';
 
 fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-    method: 'POST',
-    body: JSON.stringify({
-        name: 'foo',
-    }),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-    },
+  method: 'POST',
+  body: JSON.stringify({
+    name: 'foo',
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
 })
-    .then((response) => response.json())
-    .then((json) => {
-        let gameId = json.result.split(" ")[3];
-        let formHtml = `
+  .then((response) => response.json())
+  .then((json) => {
+    const gameId = json.result.split(' ')[3];
+    const formHtml = `
             <h2>Add your score</h2>
             <form action="https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores"
             method="post" id="name-socre-form">
@@ -20,47 +20,45 @@ fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
             <input type="text" name="score" placeholder="Your score" id="input-score"><br>
             <input type="submit" value="Submit" id="name-socre-submit">
             </form>
-        `
-        const formDiv = document.querySelector("#add-score");
-        formDiv.innerHTML = formHtml;
+        `;
+    const formDiv = document.querySelector('#add-score');
+    formDiv.innerHTML = formHtml;
 
-        const socreFrom = document.querySelector("#name-socre-form");
-        const nameInput = document.querySelector("#input-name");
-        const scoreInput = document.querySelector("#input-score");
+    const socreFrom = document.querySelector('#name-socre-form');
+    const nameInput = document.querySelector('#input-name');
+    const scoreInput = document.querySelector('#input-score');
 
-        socreFrom.addEventListener("submit", (e) => {
-            e.preventDefault();
+    socreFrom.addEventListener('submit', (e) => {
+      e.preventDefault();
 
-            fetch(socreFrom.action, {
-                method: 'POST',
-                body: JSON.stringify({
-                    user: nameInput.value,
-                    score: scoreInput.value,
-                }),
-                headers: {
-                    'Content-type': 'application/json; charset=UTF-8',
-                },
-            })
-                .then((response) => response.json())
-                .then((json) => console.log(json));
-        })
-
-        const refreshBtn = document.querySelector("#refresh-btn");
-        const nameScoreList = document.querySelector("#name-score-list");
-        refreshBtn.addEventListener("click", () => {
-            fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`)
-                .then(response => response.json())
-                .then(json => {
-                    let listHtml = "";
-                    json.result.forEach((item) => {
-                        listHtml += `
-                        <li>${item.user}:${item.score}</li>
-                        `
-                    })
-                    nameScoreList.innerHTML = listHtml;
-                    console.log(json.result)
-                });
-        })
+      fetch(socreFrom.action, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: nameInput.value,
+          score: scoreInput.value,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
     });
 
-
+    const refreshBtn = document.querySelector('#refresh-btn');
+    const nameScoreList = document.querySelector('#name-score-list');
+    refreshBtn.addEventListener('click', () => {
+      fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`)
+        .then((response) => response.json())
+        .then((json) => {
+          let listHtml = '';
+          const jsonArr = json.result;
+          jsonArr.forEach((item) => {
+            listHtml += `
+                        <li>${item.user}:${item.score}</li>
+                        `;
+          });
+          nameScoreList.innerHTML = listHtml;
+        });
+    });
+  });
