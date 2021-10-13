0a09f4f078710eaf41b29cfec5676f5e32f56223
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-13 14:49:13 +0100

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import './style.css';
 
-let gameId = "";
+let gameId = '';
 
-let createGame = async () => {
+const createGame = async () => {
+  const response = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    body: JSON.stringify({
+      name: 'foo',
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  response.json().then((json) => {
+    const [, , , c] = json.result.split(' ');
+    gameId = c;
+  });
+};
 
-    await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-        method: 'POST',
-        body: JSON.stringify({
-            name: 'foo',
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => {
-            gameId = json.result.split(' ')[3];
-        });
-}
-
-let createForm = () => {
-    const formHtml = `
+const createForm = () => {
+  const formHtml = `
             <h2>Add your score</h2>
             <form action="https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores"
             method="post" id="name-socre-form">
@@ -29,44 +28,38 @@ let createForm = () => {
             <input type="submit" value="Submit" id="name-socre-submit">
             </form>
         `;
-    const formDiv = document.querySelector('#add-score');
-    formDiv.innerHTML = formHtml;
-}
+  const formDiv = document.querySelector('#add-score');
+  formDiv.innerHTML = formHtml;
+};
 
+const addScore = async (name0, score0) => {
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name0,
+      score: score0,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  return response.json();
+};
 
-
-let addScore = async (name0, score0) => {
-    await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, {
-        method: 'POST',
-        body: JSON.stringify({
-            user: name0,
-            score: score0,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((response) => response.json())
-        .then((json) => { return });
-}
-
-
-let refreshSocre = async () => {
-    const nameScoreList = document.querySelector('#name-score-list');
-    await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`)
-        .then((response) => response.json())
-        .then((json) => {
-            let listHtml = '';
-            const jsonArr = json.result;
-            jsonArr.forEach((item) => {
-                listHtml += `
+const refreshSocre = async () => {
+  const nameScoreList = document.querySelector('#name-score-list');
+  const response = await fetch(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`);
+  response.json().then((json) => {
+    let listHtml = '';
+    const jsonArr = json.result;
+    jsonArr.forEach((item) => {
+      listHtml += `
                         <li>${item.user}:${item.score}</li>
                         `;
-            });
-            nameScoreList.innerHTML = listHtml;
-        });
-
-}
+    });
+    nameScoreList.innerHTML = listHtml;
+  });
+};
 
 createGame();
 
@@ -75,12 +68,12 @@ createForm();
 const scoreForm = document.querySelector('#name-socre-form');
 
 scoreForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    addScore(scoreForm.name.value, scoreForm.score.value);
+  e.preventDefault();
+  addScore(scoreForm.name.value, scoreForm.score.value);
 });
 
 const refreshBtn = document.querySelector('#refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
-    refreshSocre();
+  refreshSocre();
 });
