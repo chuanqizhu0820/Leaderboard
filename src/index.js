e461a2d94e96918b84bc323143c801c409c3254c
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-14 10:54:10 +0100

    Add styling using boostrap

--- src/index.js
+++ src/index.js
@@ -4,62 +4,66 @@ let gameId = '';
 const baseUri = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
 const createGame = async () => {
-  const response = await fetch(baseUri, {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'foo',
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  response.json().then((json) => {
-    const [, , , c] = json.result.split(' ');
-    gameId = c;
-  });
+    const response = await fetch(baseUri, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: 'foo',
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    response.json().then((json) => {
+        const [, , , c] = json.result.split(' ');
+        gameId = c;
+    });
 };
 
 const createForm = () => {
-  const formHtml = `
-            <h2>Add your score</h2>
+    const formHtml = `
+            <h4 class="mb-3">Add your score</h4>
             <form action= ${baseUri}+${gameId}+"/scores"
             method="post" id="name-score-form">
-            <input type="text" name="name" placeholder="Your name" id="input-name" required><br>
-            <input type="number" name="score" placeholder="Your score" id="input-score" required><br>
-            <input type="submit" value="Submit" id="name-score-submit">
+            <div class="mb-3">
+            <input type="text" name="name" placeholder="Your name" id="input-name" class="form-control" aria-describedby="emailHelp" required>
+            </div>
+            <div class="mb-3">
+            <input type="number" name="score" placeholder="Your score" class="form-control" id="input-score" required>
+            </div>
+            <input type="submit" class="btn btn-outline-secondary" value="Submit" id="name-score-submit">
             </form>
         `;
-  const formDiv = document.querySelector('#add-score');
-  formDiv.innerHTML = formHtml;
+    const formDiv = document.querySelector('#add-score');
+    formDiv.innerHTML = formHtml;
 };
 
 const addScore = async (name0, score0) => {
-  const response = await fetch(`${baseUri}${gameId}/scores`, {
-    method: 'POST',
-    body: JSON.stringify({
-      user: name0,
-      score: score0,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  return response.json();
+    const response = await fetch(`${baseUri}${gameId}/scores`, {
+        method: 'POST',
+        body: JSON.stringify({
+            user: name0,
+            score: score0,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    return response.json();
 };
 
 const refreshScore = async () => {
-  const nameScoreList = document.querySelector('#name-score-list');
-  const response = await fetch(`${baseUri}${gameId}/scores`);
-  response.json().then((json) => {
-    let listHtml = '';
-    const jsonArr = json.result;
-    jsonArr.forEach((item) => {
-      listHtml += `
-                        <li>${item.user}:${item.score}</li>
+    const nameScoreList = document.querySelector('#name-score-list');
+    const response = await fetch(`${baseUri}${gameId}/scores`);
+    response.json().then((json) => {
+        let listHtml = '';
+        const jsonArr = json.result;
+        jsonArr.forEach((item) => {
+            listHtml += `
+                        <li class="list-group-item">${item.user}: ${item.score}</li>
                         `;
+        });
+        nameScoreList.innerHTML = listHtml;
     });
-    nameScoreList.innerHTML = listHtml;
-  });
 };
 
 createGame();
@@ -69,12 +73,12 @@ createForm();
 const scoreForm = document.querySelector('#name-score-form');
 
 scoreForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addScore(scoreForm.name.value, scoreForm.score.value);
+    e.preventDefault();
+    addScore(scoreForm.name.value, scoreForm.score.value);
 });
 
 const refreshBtn = document.querySelector('#refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
-  refreshScore();
+    refreshScore();
 });
