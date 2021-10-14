ede34c3e4c262da73581ad83ada34e0df7e2c323
chuanqizhu0820 <chuanqizhu1991@outlook.com>
2021-10-14 13:26:41 +0100

    Change the styling accroding to the review comments

--- src/index.js
+++ src/index.js
@@ -4,23 +4,23 @@ let gameId = '';
 const baseUri = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
 
 const createGame = async () => {
-  const response = await fetch(baseUri, {
-    method: 'POST',
-    body: JSON.stringify({
-      name: 'foo',
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  response.json().then((json) => {
-    const [, , , c] = json.result.split(' ');
-    gameId = c;
-  });
+    const response = await fetch(baseUri, {
+        method: 'POST',
+        body: JSON.stringify({
+            name: 'foo',
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    response.json().then((json) => {
+        const [, , , c] = json.result.split(' ');
+        gameId = c;
+    });
 };
 
 const createForm = () => {
-  const formHtml = `
+    const formHtml = `
             <h4 class="mb-3">Add your score</h4>
             <form action= ${baseUri}+${gameId}+"/scores"
             method="post" id="name-score-form">
@@ -33,37 +33,39 @@ const createForm = () => {
             <input type="submit" class="btn btn-outline-secondary" value="Submit" id="name-score-submit">
             </form>
         `;
-  const formDiv = document.querySelector('#add-score');
-  formDiv.innerHTML = formHtml;
+    const formDiv = document.querySelector('#add-score');
+    formDiv.innerHTML = formHtml;
 };
 
 const addScore = async (name0, score0) => {
-  const response = await fetch(`${baseUri}${gameId}/scores`, {
-    method: 'POST',
-    body: JSON.stringify({
-      user: name0,
-      score: score0,
-    }),
-    headers: {
-      'Content-type': 'application/json; charset=UTF-8',
-    },
-  });
-  return response.json();
+    const response = await fetch(`${baseUri}${gameId}/scores`, {
+        method: 'POST',
+        body: JSON.stringify({
+            user: name0,
+            score: score0,
+        }),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    });
+    return response.json();
 };
 
 const refreshScore = async () => {
-  const nameScoreList = document.querySelector('#name-score-list');
-  const response = await fetch(`${baseUri}${gameId}/scores`);
-  response.json().then((json) => {
-    let listHtml = '';
-    const jsonArr = json.result;
-    jsonArr.forEach((item) => {
-      listHtml += `
-                        <li class="list-group-item">${item.user}: ${item.score}</li>
-                        `;
+    const nameScoreList = document.querySelector('#name-score-list');
+    const response = await fetch(`${baseUri}${gameId}/scores`);
+    response.json().then((json) => {
+        let listHtml = '';
+        const jsonArr = json.result;
+        jsonArr.forEach((item, i) => {
+            if (i % 2 == 0) {
+                listHtml += `<li class="list-group-item">${item.user}: ${item.score}</li>`;
+            } else {
+                listHtml += `<li class="list-group-item list-group-item-secondary">${item.user}: ${item.score}</li>`;
+            }
+        });
+        nameScoreList.innerHTML = listHtml;
     });
-    nameScoreList.innerHTML = listHtml;
-  });
 };
 
 createGame();
@@ -73,12 +75,12 @@ createForm();
 const scoreForm = document.querySelector('#name-score-form');
 
 scoreForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  addScore(scoreForm.name.value, scoreForm.score.value);
+    e.preventDefault();
+    addScore(scoreForm.name.value, scoreForm.score.value);
 });
 
 const refreshBtn = document.querySelector('#refresh-btn');
 
 refreshBtn.addEventListener('click', () => {
-  refreshScore();
+    refreshScore();
 });
